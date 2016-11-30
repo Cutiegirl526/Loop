90c49f754e16c787e8b57fcc522a9bef315cf639
Cutiegirl526 <Kayladiniega@gmail.com>
2016-11-29 20:46:43 -1000

    Stitch's family

---
+++ Loop_city.js
@@ -0,0 +1,30 @@
+// Mommy I can counts to 100 in Javascript
+
+var count;
+
+for (var count = 0; count <=100; count++){
+  console.log("I can count to " + count);
+}
+
+// Noah Arc
+
+var animalCount;
+
+for (var animalCount = 0; animalCount <=5; animalCount++){
+  console.log(animalCount);
+  console.log("there are " + animalCount + " many animals in Noahs Arc");
+}
+
+
+//Hip Hop Array
+
+var partyAnimals = ['Experiment 626', 'Experiment 523', 'Experiment 062', 'Experiment 601', 'Experiment 625'];
+
+for (var imin = 0; imin <= 4; imin++){
+  console.log (partyAnimals[imin]);
+}
+
+
+//Merlin's Battle Weapon
+
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"]
